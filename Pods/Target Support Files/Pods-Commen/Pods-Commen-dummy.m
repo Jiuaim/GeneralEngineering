6c6d4a393ab297b125baac4c7a13cf127afd9737
hsz <shengzhong.huang@zhangmen.com>
2019-12-25 10:19:50 +0800

    git ignore

--- Pods/Target Support Files/Pods-Commen/Pods-Commen-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Commen : NSObject
-@end
-@implementation PodsDummy_Pods_Commen
-@end
--- Pods/Target Support Files/Pods-General/Pods-General-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_General : NSObject
-@end
-@implementation PodsDummy_Pods_General
-@end
